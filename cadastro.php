27bc0c79d94267a76733533f8006d2d62441e4f4
Roberta Silvano <robertaspb@gmail.com>
2021-03-15 20:47:39 -0300

    arrumado o layout da página de cadastro

--- cadastro.php
+++ cadastro.php
@@ -79,44 +79,58 @@
         ?>
             <div class="row">
                 <div class="container-cadastro">
-                    <form method="post" action="">
+                    <form  method="post" action="" class="form-cadastro">
                         <h1 class="h3 mb-3 fw-bold">Cadastrar atleta</h1>
-                        <div class="col-md-6">
-                            <label for="nome" class="visually-hidden">Nome</label>
-                            <input type="text" id="nome" name="nome" class="form-control" placeholder="Nome"  required autofocus>
-                            <label for="user" class="visually-hidden">Username</label>
-                            <input type="text" id="user" name="user" class="form-control" placeholder="Username" require>
-                            <label for="password" class="visually-hidden">Password</label>
-                            <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
-                            <label for="nascimento" class="visually-hidden">Nascimento</label>
-                            <input type="date" id="nascimento" name="nascimento" class="form-control" placeholder="Nascimento" require>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label for="nome" class="visually-hidden">Nome</label>
+                                <input type="text" id="nome" name="nome" class="form-control" placeholder="Nome"  required autofocus>
+                                <label for="user" class="visually-hidden">Username</label>
+                                <input type="text" id="user" name="user" class="form-control" placeholder="Username" require>
+                                <label for="password" class="visually-hidden">Password</label>
+                                <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
+                                <label for="nascimento" class="visually-hidden">Nascimento</label>
+                                <input type="date" id="nascimento" name="nascimento" class="form-control" placeholder="Nascimento" require>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                <label for="telefone" class="visually-hidden">Telefone</label>
+                                <input type="tel" id="telefone" name="telefone" class="form-control" placeholder="Telefone: 11-11111-1111" pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}" require>
+                                <label for="convenio" class="visually-hidden">Convênio</label>
+                                <input type="text" id="convenio" name="convenio" class="form-control" placeholder="Convênio">
+                                <label for="tipo_sangue" class="visually-hidden">Tipo sanguíneo</label>
+                                <input type="text" id="tipo_sangue" name="tipo_sangue" class="form-control" placeholder="Tipo sanguíneo" require>
+                                <label for="cpf" class="visually-hidden">CPF</label>
+                                <input type="text" id="cpf" name="cpf" class="form-control" placeholder="CPF: 111.111.111-11" pattern="[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}" require>
+                            </div>
                         </div>
-                        
-                        <div class="col-md-6">
-                            <label for="telefone" class="visually-hidden">Telefone</label>
-                            <input type="tel" id="telefone" name="telefone" class="form-control" placeholder="Telefone: 11-11111-1111" pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}" require>
-                            <label for="convenio" class="visually-hidden">Convênio</label>
-                            <input type="text" id="convenio" name="convenio" class="form-control" placeholder="Convênio">
-                            <label for="tipo_sangue" class="visually-hidden">Tipo sanguíneo</label>
-                            <input type="text" id="tipo_sangue" name="tipo_sangue" class="form-control" placeholder="Tipo sanguíneo" require>
-                            <label for="cpf" class="visually-hidden">CPF</label>
-                            <input type="text" id="cpf" name="cpf" class="form-control" placeholder="CPF: 111.111.111-11" pattern="[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}" require>
-    
-                            <label for="diretoria" class="visually-hidden">Membro da diretoria</label>
-                            <select id="diretoria" name="diretoria" class="form-control" require>
-                                <option value="sim">É membro da diretoria</option>
-                                <option value="nao" selected>Não é membro da diretoria</option>
-                            </select>
-                        </div>
-                        <br>
-                        <button class="w-100 btn btn-outline-warning" type="submit" name='login'>Login</button>
+                        <input type="checkbox" id="diretoria" name="diretoria">
+                        <label for="diretoria">Membro da diretoria</label>
+                        <button class="w-100 btn btn-outline-warning" type="submit" name='cadastrar'>Cadastrar</button>
                     </form>
+                    <!-- se a SESSION já estiver setada, significa que já foi tentado realizar login, e então da um alert de qual foi o erro. -->
+                    <?php 
+                            if (isset($_SESSION['user'])) {
+                                if ($_SESSION['user'] == '' && $_SESSION['password'] == 'password_invalido') {
+                                    echo "<br><div class='alert alert-warning'> senha inválida! </div>";
+                                }
+                                else if ($_SESSION['user'] == 'login_invalido' || $_SESSION['password'] == 'password_invalido') {
+                                    echo "<br><div class='alert alert-warning'> user inválido! </div>";
+                                }
+                                else {
+                                    echo "<br><div class='alert alert-warning'> erro ao cadastrar atleta! </div>";
+                                }
+                            }
+                    ?>
                 </div>
             </div>
 
+            <hr>
+            
             <div class="row">
                 <div class="container-tabela">
-                    <table>
+                <h1 class="h3 mb-3 fw-bold">Atletas cadastrados</h1>
+                    <table class="table table-striped table-warning table-bordered table-hover">
                         <thead>
                             <tr>
                                 <th>Id</th>
@@ -134,6 +148,22 @@
                         </thead>
         
                         <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Roberta Silvano</td>
+                                <td>robs</td>
+                                <td>adjdiasdjisd</td>
+                                <td>1996-07-01</td>
+                                <td>11111111111</td>
+                                <td>SC</td>
+                                <td>O+</td>
+                                <td>11111111111</td>
+                                <td>Sim</td>
+                                <td>
+                                    <a href="#">Editar</a>
+                                    <a href="#">Excluir</a>
+                                </td>
+                            </tr>
                             <tr>
                                 <td>a</td>
                                 <td>a</td>
@@ -156,20 +186,6 @@
                 </div>
             </div>
           
-            <!-- se a SESSION já estiver setada, significa que já foi tentado realizar login, e então da um alert de qual foi o erro. -->
-            <?php 
-                            if (isset($_SESSION['user'])) {
-                                if ($_SESSION['user'] == '' && $_SESSION['password'] == 'password_invalido') {
-                                    echo "<br><div class='alert alert-warning'> senha inválida! </div>";
-                                }
-                                else if ($_SESSION['user'] == 'login_invalido' || $_SESSION['password'] == 'password_invalido') {
-                                    echo "<br><div class='alert alert-warning'> user inválido! </div>";
-                                }
-                                else {
-                                    echo "<br><div class='alert alert-warning'> erro ao fazer login! </div>";
-                                }
-                            }
-            ?>
        </main>
     </div>
 
